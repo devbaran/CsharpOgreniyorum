c93f55b42830c070868e67e110c2bc7c60f90947
Baran Kurtbogan <barankurtbogan@gmail.com>
2021-02-19 15:34:43 +0300

    19 subat

---
+++ Class/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+        /* get ve set terimleri nereden gelir, bir propa tanım yaparken örnek olarak 
+         * FirstName propuna Baran tanımlarken bunu SET etmiş oluyoruz, ek olarak bu tanımladığımız Baran propunu
+         * kullanırken veya görmek için kullanmaya çalışırken bunu GET etmiş oluyoruz...
+        */
+
+
+
+
+        // get ve setler alınan veriyi işlerken değiştirmek için de kullanılabilir.
+        private string _tcNo; // değişken tanımladık
+        public string TcN
+        {
+            get { return "Tc Numara: " + _tcNo; }
+            set { _tcNo = value; }
+        }
+        /* set ile aldığımız tc yi _tcNo değişkenine atadık, GET ederken 
+         yani kullanırken başına Tc Numara: ibaresi getirdik bu sayede işlemiş olduk ancak bu kısım
+        nadiren kullanılan bir yoldur, genellikle üst kısımda bulunan prop tiplerini kullanacağız,
+        bu tarz kullanımlar artık geçmişte kaldı ... */
+
+
+
+
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer(); // prop classımızdan bir örnek yaptık....
+            customer.City = "Batman";
+            customer.Id = 1;
+            customer.FirstName = "Memduh";
+            customer.LastName = "Kurtboğan";
+
+            Customer customer1 = new Customer { City = "Batman" ,FirstName="Baran", LastName="Kurtbogan",Id=1 };
+            //bu şekilde prop kullanımı da mevcuttur...
+
+            Console.WriteLine(customer1.FirstName); // c1'in adını yazdıralım
+
+        }
+    }
+
+    //AŞAĞIDAKİ KISIMLAR DEVAMINDA DİĞER SEKMELERE EKLENDİ
+
+    //class CustomerManager /* classlar ayrı olarak tanımlanır ancak biz burada tek sayfaya yazdık örnek amacıyla
+    //    buradaki Manager olduğu için burası bir fonksiyon classıdır biz buradaki classın fonksiyonlarını 
+    //    kullanmak istiyorsak yukarıda programın çalışacağı yerde onları bir kere örnek/ref. göstermeliyiz.*/
+    //{
+    //    public void Add()
+    //    {
+    //        Console.WriteLine("Customer Added!");
+    //    }
+    //    public void Update()
+    //    {
+    //        Console.WriteLine("Customer Updated!");
+    //    }
+    //}
+    //class ProductManager/* bir class daha tanımladık, temel anlamda biz yapacaklarımızı gruplandırmış olduk
+    //                     üsteki classta customerlar hakkında işlemler yaparken burada product hakkında işlemler yapıyoruz
+    //    aynı şekilde bunu da kullanmak için yukarıda  tanımlayalım...*/
+    //{
+    //    public void Add()
+    //    {
+    //        Console.WriteLine("Product Added!");
+    //    }
+    //    public void Update()
+    //    {
+    //        Console.WriteLine("Product Updated");
+    //    }
+    //}
+}
